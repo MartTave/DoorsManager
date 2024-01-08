4fb4e18a32bfd18577b7684b8e8afe83a54361be
MartTave <martin.tavernier256@gmail.com>
2024-01-08 14:55:05 +0100

    Added base page for backend development and testing

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,21 @@
 import Express from "express"
-
-
+import {join} from "path"
+const pagePath = "../frontend/www/"
 const app = Express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/login/', (req, res) => {
+  res.sendFile(join(__dirname, pagePath + '/login.html'));
+  console.log(req.params)
+  if(req.query.username) {
+    // Erreur login invalid
+    console.log(req.query.username)
+  }
 })
 
 app.listen(port, () => {
